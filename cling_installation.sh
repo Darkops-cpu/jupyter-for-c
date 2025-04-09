387553356c9ae38338f4bdef8203642dfc56f49d
Jayant Yadav <darksoul00900@gmail.com>
2025-04-09 16:45:11 +0530

    Update cling_installation.sh

--- cling_installation.sh
+++ cling_installation.sh
@@ -1,1 +1,17 @@
+#!bin/bash
+# be usre to have jupyter in your system
+# to install miniconda (change upon ur os)
 
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# close current session after completion, new terminal
+
+source ~/.bashrc   #activates conda
+
+#create conda env and install cling
+
+conda create -n cling_env -c conda-forge xeus-cling notebook jupyterlab
+conda activate cling_env
+
+# launch jupyter labs, there inder NEW FILES you will come across the thing you had been hopping for, so not run heavy c codes that surpass the base C9/C11. 
